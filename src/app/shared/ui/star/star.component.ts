e80c114244154fc6f3a1f0b0423204f4853c9b3f
bennnou <benjamin.longearet@elium.io>
2020-05-25 11:40:05 +0200

    step-07 / 2. use input for starComponent

--- src/app/shared/ui/star/star.component.ts
+++ src/app/shared/ui/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnInit {
+
+  @Input() rating: number = 3
 
   constructor() { }
 
