e028161e8b964770b2066a646bd659888da86c5e
bennnou <benjamin.longearet@elium.io>
2020-05-28 11:56:20 +0200

    step-11 / 2.1.2 Define products route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductComponent } from './product/product.component';
 
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent }
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'products', component: ProductComponent, children: []}
 ];
 
 @NgModule({
