4233090c01fb9046ccbf74581ab3882935ccb415
bennnou <benjamin.longearet@elium.io>
2020-05-29 10:46:49 +0200

    step-13 / 1. Import ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { ProductEditComponent } from './product/product-edit/product-edit.compon
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
