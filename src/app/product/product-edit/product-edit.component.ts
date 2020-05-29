a2bf785ffd5dde3c13cab80d5d019b4b1fe996f7
bennnou <benjamin.longearet@elium.io>
2020-05-29 10:59:18 +0200

    step-13 / 4. Create the productForm

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const HTTP_URL_PATTERN: string = '^((http[s]?):\\/)\\/?([^:\\/\\s]+)((\\/\\w+)*)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$'
 
 @Component({
   selector: 'app-product-edit',
@@ -10,7 +12,26 @@ export class ProductEditComponent implements OnInit {
 
   public productForm: FormGroup
 
-  constructor(fb: FormBuilder) { }
+  constructor(fb: FormBuilder) {
+    // We create our Form for product
+    this.productForm = fb.group({
+      id: [null], // It is the same as `id: new FormControl(null)`
+      productName: [
+        '', // default value
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(80)
+        ] // All the validators to run against this field
+      ],
+      productCode: [''],
+      releaseDate: [new Date()],
+      description: [''],
+      price: [0, Validators.min(0)],
+      starRating: [3, [Validators.min(0), Validators.max(5)]],
+      imageUrl: ['', Validators.pattern(HTTP_URL_PATTERN)]
+    })
+  }
 
   ngOnInit(): void {
   }
